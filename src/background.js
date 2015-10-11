ff0641e6c01c4c9e130c2a14a089b6eccd3eb156
cf256 <cflinchausen@gmail.com>
2015-10-11 15:29:44 +1100

    Initial Commit
    First draft

---
+++ src/background.js
@@ -0,0 +1,147 @@
+//amedia am style
+//TODO make into one insertion
+var articleEntryArray= ["http://www.ammta.no/"];
+articleEntryArray.push("http://www.retten.no/");
+articleEntryArray.push("http://www.auraavis.no/");
+articleEntryArray.push("http://www.austagderblad.no/");
+articleEntryArray.push("http://www.nordhordland.no/");
+articleEntryArray.push("http://www.an.no/");
+articleEntryArray.push("http://www.bygdeposten.no/");
+articleEntryArray.push("http://www.eikerbladet.no/");
+articleEntryArray.push("http://www.enebakkavis.no/");
+articleEntryArray.push("http://www.firda.no/");
+articleEntryArray.push("http://www.firdaposten.no/");
+articleEntryArray.push("http://www.fremover.no/");
+articleEntryArray.push("http://www.hadeland.no/");
+articleEntryArray.push("http://www.hardanger-folkeblad.no/");
+articleEntryArray.push("http://www.helg.no/");
+articleEntryArray.push("http://www.indre.no/");
+articleEntryArray.push("http://www.kv.no/");
+articleEntryArray.push("http://www.kvinnheringen.no/");
+articleEntryArray.push("http://www.laagendalsposten.no/");
+articleEntryArray.push("http://www.lierposten.no/");
+articleEntryArray.push("http://www.lofotposten.no/");
+articleEntryArray.push("http://www.lofot-tidende.no/");
+articleEntryArray.push("http://www.oyene.no/");
+articleEntryArray.push("http://www.noblad.no/");
+articleEntryArray.push("http://www.pd.no/");
+articleEntryArray.push("http://www.r-a.no/");
+articleEntryArray.push("http://www.ranablad.no/");
+articleEntryArray.push("http://www.ringblad.no/");
+articleEntryArray.push("http://www.ringsaker-blad.no/");
+articleEntryArray.push("http://www.rha.no/");
+articleEntryArray.push("http://www.sandeavis.no/");
+articleEntryArray.push("http://www.smaalenene.no/");
+articleEntryArray.push("http://www.svelvikposten.no/");
+articleEntryArray.push("http://www.telen.no/");
+articleEntryArray.push("http://www.tk.no/");
+articleEntryArray.push("http://www.tvedestrandsposten.no/");
+articleEntryArray.push("http://www.vestbyavis.no/");
+articleEntryArray.push("http://www.aasavis.no/");
+
+// amedia df style:
+// one insertion
+var dfSkinPaywall = ["http://www.ba.no/"];
+dfSkinPaywall.push("http://www.dt.no/");
+dfSkinPaywall.push("http://www.ifinnmark.no/");
+dfSkinPaywall.push("http://www.f-b.no/");
+dfSkinPaywall.push("http://www.gjengangeren.no/");
+dfSkinPaywall.push("http://www.glomdalen.no/");
+dfSkinPaywall.push("http://www.ha-halden.no/");
+dfSkinPaywall.push("http://www.h-avis.no/");
+dfSkinPaywall.push("http://www.jarlsbergavis.no/");
+dfSkinPaywall.push("http://www.moss-avis.no/");
+dfSkinPaywall.push("http://www.nordlys.no/");
+dfSkinPaywall.push("http://www.oa.no/");
+dfSkinPaywall.push("http://www.rb.no/");
+dfSkinPaywall.push("http://www.sb.no/");
+dfSkinPaywall.push("http://www.sa.no/");
+dfSkinPaywall.push("http://www.ta.no/");
+dfSkinPaywall.push("http://www.tb.no/");
+dfSkinPaywall.push("http://www.oblad.no/");
+dfSkinPaywall.push("http://www.op.no/");
+dfSkinPaywall.push("http://www.ostlendingen.no/");
+
+var url = document.URL;
+if(url.indexOf("bt.no") >= 0 || url.indexOf("aftenbladet.no") >= 0 || url.indexOf("fvn.no") >= 0){ 
+	console.log("in bt/aftenbladet");
+	var divs = document.getElementsByClassName("df-skin-paywall-closed");
+	for(var i = 0; i < divs.length; i++){
+		divs[i].style.display = 'none';
+		console.log("hit");
+	}
+} else if(url.indexOf("vg.no") >= 0) {
+	console.log("in VG");
+	var divs = document.getElementsByClassName("article-extract");
+	for(var i = 0; i < divs.length; i++) {
+		if (divs[i].hasChildNodes()) {
+			var spans = divs[i].querySelectorAll("span");
+			for(var j = 0; j < spans.length; j++) {
+				if(spans[j].classList.contains("df-img-skin-pluss")){
+					divs[i].style.display = 'none';
+					console.log("hit");
+				}
+			}
+		}
+	}
+	var vgPlussTeaser = document.getElementById("pluss-teaser");
+	vgPlussTeaser.style.display = 'none';
+} else if (url.indexOf("dagbladet.no") >= 0) {
+	console.log("in DB");
+	var articles = document.getElementsByClassName("preview");
+	for(var i = 0; i < articles.length; i++) {
+		if (articles[i].hasChildNodes()) {
+			var child = articles[i].children[0];
+			var href = child.getAttribute("href");
+			if (href.indexOf("/pluss/") >= 0) {
+				articles[i].style.display = 'none';
+				console.log("hit");
+			}
+		}
+	}
+} else if (url.indexOf("aftenposten.no") >= 0) {
+	console.log("in AP");
+	var divs = document.getElementsByClassName("df-skin-art-Feat-Amagasinet");
+	for(var i = 0; i < divs.length; i++){
+		divs[i].style.display = 'none';
+		console.log("hit");
+	}
+} else if (url.indexOf("dn.no") >= 0) {
+	//todo d2
+	console.log("in DN");
+	var divs = document.getElementsByClassName("df-skin-paid");
+	for(var i = 0; i < divs.length; i++){
+		divs[i].style.display = 'none';
+		console.log("hit");
+	}	
+} else if (url.indexOf("adressa.no") >= 0) {
+	console.log("in adressa");
+	var divs = document.getElementsByClassName("article payed");
+	for(var i = 0; i < divs.length; i++) {
+		divs[i].style.display = 'none';
+		console.log("hits");
+	}
+} else if (articleEntryArray.indexOf(url) != -1){
+	console.log("am-articleEntry");
+	var divs = document.getElementsByClassName("am-articleEntry");
+	for(var i = 0; i < divs.length; i++) {
+		if (divs[i].hasChildNodes()) {
+			var spans = divs[i].querySelectorAll("span");
+			for(var j = 0; j < spans.length; j++) {
+				if(spans[j].classList.contains("am-premium-logo")){
+					divs[i].style.display = 'none';
+					console.log("hit");
+				}
+			}
+		}
+	}
+} else if (dfSkinPaywall.indexOf(url) != -1){
+	console.log("dfSkinPaywall");
+	var divs = document.getElementsByClassName("df-skin-paywall");
+	for(var i = 0; i < divs.length; i++){
+		divs[i].style.display = 'none';
+		console.log("hit");
+	}
+} else {
+	console.log(url + "NO HIT	");
+}
